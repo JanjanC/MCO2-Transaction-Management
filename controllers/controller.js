5b597cc63f93514fcc9b91334a09482a08762c31
JanjanC <janjan_chua@dlsu.edu.ph>
2022-01-27 18:54:56 +0800

    Initial UI

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,6 +2,18 @@ const controller = {
     getIndex: function (req, res) {
         res.render('index');
     },
+
+    getViewPage: function (req, res) {
+        res.render('view');
+    },
+
+    getCreatePage: function (req, res) {
+        res.render('create');
+    },
+
+    getEditPage: function (req, res) {
+        res.render('edit');
+    },
 };
 
 module.exports = controller;
--- routes/routes.js
+++ routes/routes.js
@@ -4,5 +4,8 @@ const controller = require('../controllers/controller.js');
 const app = express();
 
 app.get('/', controller.getIndex);
+app.get('/view', controller.getViewPage);
+app.get('/create', controller.getCreatePage);
+app.get('/edit', controller.getEditPage);
 
 module.exports = app;
